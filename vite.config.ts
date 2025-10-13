b6a39324e308c97f05f73988516dc0e7178e0a60
Jordy Villamil <jordyvillamilletras@gmail.com>
2025-10-12 21:42:24 -0500

    se realizan pruebas para corregir el error en el archivo main

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,13 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path' 
+import path from 'path'
 
 export default defineConfig({
-  base: './',
+  base: './',  // Funciona tanto en dev como en producción
   plugins: [react()],
   resolve: {
     alias: {
-      // Reemplaza "framer-motion" con la ruta absoluta al paquete en node_modules
       'framer-motion': path.resolve(__dirname, 'node_modules/framer-motion'),
     },
   },
-
-  // La propiedad 'base' debe seguir eliminada si estás usando el repositorio de usuario (JordyVillamil.github.io)
 })
