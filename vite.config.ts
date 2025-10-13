7135bcfa88f798c9f4573469bb4612559c45c426
Jordy Villamil <jordyvillamilletras@gmail.com>
2025-10-12 23:36:56 -0500

    se realizan actualizaciones a los archivos package.json y vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
-  base: '/portfolio-Jordy-Villamil/',  // Nombre de tu repositorio
   plugins: [react()],
-  resolve: {
-    alias: {
-      'framer-motion': path.resolve(__dirname, 'node_modules/framer-motion'),
-    },
-  },
+  base: '/portfolio-Jordy-Villamil/',
 })
