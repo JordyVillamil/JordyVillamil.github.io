25d9a506b266d96c9457afb3bcf2599cc0ae920e
Jordy Villamil <jordyvillamilletras@gmail.com>
2025-10-14 13:24:31 -0500

    se actualizan fuentes en homesection

--- src/sections/HomeSection.tsx
+++ src/sections/HomeSection.tsx
@@ -33,10 +33,10 @@ const HomeSection: React.FC<SectionProps> = ({ id }) => {
           repeat={Infinity}
         />
         <h1 className="name-title">
-          JORDY FABIAN
+          JORDY
         </h1>
         <h2 className="fullstack-title">
-          VILLAMIL LETRADO
+          VILLAMIL
         </h2>
       </motion.div>
       
